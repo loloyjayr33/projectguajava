88e930c9479fddad9d849a2648d67cd1a3e82568
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-03-11 16:00:01 +0800

    [pages] Add initial Vue application routes

---
+++ pages/support.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"> 
+const title = ref("ProjectGuajava | Home"); 
+const description = ref("A community based Payroll Management System"); 
+ 
+useSeoMeta({ 
+  title: () => title.value, 
+  description: () => `${title.value} - ${description.value}`, 
+  charset: "utf-8", 
+  viewport: "width=device-width, initial-scale=1.0", 
+  robots: "index, follow", 
+ 
+  ogTitle: () => title.value, 
+  ogDescription: () => `${title.value} - ${description.value}`, 
+ 
+  // ogImage: "https://projectguavajava.vercel.app/featured.jpg", 
+  // twitterCard: "summary_large_image", 
+}); 
+</script>
+
+<template> 
+  <Html lang="en"> 
+      <Head> 
+      <Link rel="icon" type="image/png" href="/payment-check.png"></Link> 
+      <link href="/reset.css" rel="stylesheet" /> 
+      </Head> 
+  </Html> 
+      
+  <client-only> 
+    <Welcome /> 
+  </client-only> 
+</template> 
+
+<style scoped></style> 
