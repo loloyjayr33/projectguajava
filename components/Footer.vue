c253375495e34f7728048956f689632a8ba4b895
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-03-11 16:10:54 +0800

    [components/Footer.vue] Adding footer content to a website by putting terms, policy, support and company name

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"> 
+  const year = new Date().getFullYear(); 
+</script> 
+ 
+<template> 
+  <footer> 
+    <nav> 
+        <a href="terms">Terms and Conditions</a> 
+        <span>&nbsp;|&nbsp;</span> 
+        <a href="privacy">Privacy Policy</a> 
+        <span>&nbsp;|&nbsp;</span> 
+        <a href="support">Support</a> 
+    </nav> 
+    <p> 
+      <span>&copy;&nbsp;</span> 
+      <span>{{ year }}</span> 
+      <span>&nbsp;Guajava. All rights reserved.</span> 
+    </p> 
+  </footer> 
+</template> 
+ 
+<style scoped></style> 
