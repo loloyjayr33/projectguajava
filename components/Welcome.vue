5068eca5d35443c938368048e642277d9a412fd8
Jhon Anthony Varron <jhonanthonyvarron02@gmail.com>
2025-03-11 16:13:20 +0800

    [components/Welcome.vue] Adding welcome content to a website as a landing page

---
+++ components/Welcome.vue
@@ -0,0 +1,22 @@
+<script lang="ts"> 
+if (typeof (window as any).global === "undefined") { 
+  (window as any).global = window; 
+} 
+ 
+export default { 
+  components: true, 
+}; 
+</script> 
+ 
+<template> 
+  <client-only> 
+    <Header /> 
+    <Footer /> 
+  </client-only> 
+</template> 
+ 
+<script lang="ts" setup></script> 
+ 
+<style scoped></style> 
+ 
+<style></style>
